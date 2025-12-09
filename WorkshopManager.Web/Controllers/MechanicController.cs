324bd601d41c89b54575198462514d2d427c60e3
matij330 <matq11@onet.eu>
2025-12-09 23:36:59 +0100

    WSPCZ-17-owner-zarzadza-mechanikami-crud

--- WorkshopManager.Web/Controllers/MechanicController.cs
+++ WorkshopManager.Web/Controllers/MechanicController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using WorkshopManager.DAL.EF;
 using WorkshopManager.Model.DataModels;
 using WorkshopManager.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace WorkshopManager.Web.Controllers
 {
@@ -41,5 +43,96 @@ namespace WorkshopManager.Web.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("Index", "Owner");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var mechanics = await _context.Mechanics
+            .Select(m => new MechanicCreateViewModel
+            {
+                Id = m.Id,
+                FirstName = m.FirstName,
+                LastName = m.LastName
+            })
+            .ToListAsync();
+            return View(mechanics);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var mechanic = await _context.Mechanics.FindAsync(id);
+            if (mechanic == null)
+            {
+                return NotFound();
+            }
+
+            var model = new MechanicCreateViewModel
+            {
+                Id = mechanic.Id,
+                FirstName = mechanic.FirstName,
+                LastName = mechanic.LastName
+            };
+
+                return View(model);
+        }
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Edit(MechanicCreateViewModel model)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var mechanic = await _context.Mechanics.FindAsync(model.Id);
+                if (mechanic == null)
+                {
+                    return NotFound();
+                }
+
+                mechanic.FirstName = model.FirstName;
+                mechanic.LastName = model.LastName;
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(List));
+            }
+
+            [HttpGet]
+            public async Task<IActionResult> Delete(int id)
+            {
+                var mechanic = await _context.Mechanics.FindAsync(id);
+                if (mechanic == null)
+                {
+                    return NotFound();
+                }
+            
+                var model = new MechanicCreateViewModel
+                {
+                    Id = mechanic.Id,
+                    FirstName = mechanic.FirstName,
+                    LastName = mechanic.LastName
+                };
+            
+                return View(model);
+            }
+            
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Delete(MechanicCreateViewModel model)
+            {
+                var mechanic = await _context.Mechanics.FindAsync(model.Id);
+                if (mechanic == null)
+                {
+                    return NotFound();
+                }
+            
+                _context.Mechanics.Remove(mechanic);
+                await _context.SaveChangesAsync();
+            
+                return RedirectToAction(nameof(List));
+            }
+
     }
 }
